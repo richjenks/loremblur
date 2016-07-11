5563556a2f8a6279c9989f29c5f21ca9bc4d311d
Richard Jenkins <richard.jenkins@rivosoftware.com>
2016-07-11 11:07:27 +0100

    Welcome page styling

--- welcome.php
+++ welcome.php
@@ -13,15 +13,14 @@
 </head>
 <body>
 	<div class="container">
-		<h1><b>loremblur.io</b> is a different kind of placeholder image service</h1>
+		<h1><b>loremblur.io</b> provides neutral and inoffensive coloured placeholder images</h1>
 		<div class="content">
-			<p>Rationale.</p>
-			<hr>
 			<p>Add the size of the image you want after the URL and you'll get a placeholder:</p>
 			<a class="code" target="_blank" href="<?= $url; ?>?200x150"><?= $url; ?>?200x150</a>
-			<p>Or just use one number to get a square placeholder:</p>
-			<code>&lt;img src="<?= $url; ?>?300"&gt;</code>
 			<img src="<?= $url; ?>?200x150">
+			<p>Or just use one number to get a square placeholder:</p>
+			<code>&lt;img src="<?= $url; ?>?70"&gt;</code>
+			<img src="<?= $url; ?>?70">
 		</div>
 	</div>
 </body>
